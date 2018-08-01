33dfcce9f7b88e27a161c4bf17578dac98ad82fb
Edward Manda <lilkups@yahoo.com>
2018-08-01 09:21:04 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,44 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-import { Wrapper, H1, Content } from './AppCSS';
+import {
+	Wrapper,
+	Intro,
+	H1,
+	H2,
+	Links,
+	H3,
+	Content,
+	Container,
+	Form,
+	Input,
+	TextArea,
+	Button,
+	Description,
+} from './AppCSS';
 
 class App extends Component {
 	state = {
-		header: 'Main heading goes here',
-		head1: '',
-		head2: '',
-		head3: [],
+		head: 'Main heading goes here',
+		intro: '',
+		subHead: '',
+		links: [],
+		content: [],
 	};
 
 	makePage = (page) => {
-		const { header, head1, head2, head3 } = page;
-		console.log(head3);
+		const { head, intro, subHead, links, content } = page;
 		this.setState({
-			header,
-			head1,
-			head2,
-			head3,
+			head,
+			intro,
+			subHead,
+			links,
+			content,
 		});
 	};
 	componentDidMount() {
 		axios
-			.get('http://localhost:8000')
+			.get('http://localhost:8000/api')
 			.then((response) => {
 				this.makePage(response.data.page);
 			})
@@ -31,15 +46,55 @@ class App extends Component {
 				console.log('Error: ', err);
 			});
 	}
+
+	renderTextArea = (bol) => {
+		if (bol) {
+			return (
+				<React.Fragment>
+					Content:
+					<TextArea name={'content'} cols={80} rows={10} />
+				</React.Fragment>
+			);
+		} else {
+			return null;
+		}
+	};
+
 	render() {
 		return (
 			<Wrapper>
-				<H1>{this.state.header}</H1>
+				<H1>{this.state.head}</H1>
+				<Links>
+					{this.state.links.map((link, i) => {
+						const { url, text, description } = link;
+						return (
+							<li key={i}>
+								<i>
+									<a href={'/api/' + url}>{text}</a>
+								</i>
+								{description}.
+							</li>
+						);
+					})}
+				</Links>
+
 				<Content>
-					<h1>{this.state.head1}</h1>
-					<h2>{this.state.head2}</h2>
-					{this.state.head3.map((head3) => {
-						head3.heading;
+					<Intro>{this.state.intro}</Intro>
+					<H2>{this.state.subHead}</H2>
+					{this.state.content.map((block, i) => {
+						const { title, form: { action, method, label, type, name, textArea, description } } = block;
+						return (
+							<Container key={i}>
+								<H3>{title}</H3>
+								<Form action={'/api/' + action} method={method}>
+									{label}:
+									<Input type={type} name={name} />
+									{this.renderTextArea(textArea)}
+									<Description>{description}</Description>
+									<Button>Submit</Button>
+								</Form>
+							</Container>
+						);
 					})}
 				</Content>
 			</Wrapper>
